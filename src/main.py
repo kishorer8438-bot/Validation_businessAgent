a09071109f4e720551cd1276e57750b560341673
Kishore <kishorer8438r@gmail.com>
2026-07-19 16:38:30 +0530

    Updated project

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
-from fastapi import FastAPI     
+from fastapi import FastAPI
+
 app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the validation service!"}
+
 
 @app.post("/validate-payload")
 def validate_payload(data: dict):
